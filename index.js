0ab6e9e4dfa0d3268b42848b23f7bf7123f3f821
Tom Nielsen <tanielsen@gmail.com>
2021-10-12 14:56:27 +0100

    gen pdf?

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { getState } = require("@saltcorn/data/db/state");
 const db = require("@saltcorn/data/db");
 
 const { URL } = require("url");
+const { generatePdf } = require("html-pdf-node");
+const fs = require("fs");
 
 module.exports = {
   sc_plugin_api_version: 1,
@@ -47,41 +49,45 @@ module.exports = {
         const thePage = await Page.findOne({ name: page });
         if (thePage) {
           const contents = await thePage.run(qstate, { res: {}, req });
-          const state = getState();
-          const layout = state.getLayout(req.user);
-          const version_tag = db.connectObj.version_tag;
-
-          const headers = [
-            ...state.headers,
-            {
-              headerTag: `<script>var _sc_globalCsrf = "${req.csrfToken()}"; var _sc_version_tag = "${version_tag}";</script>`,
-            },
-            { css: `/static_assets/${version_tag}/saltcorn.css` },
-            { script: `/static_assets/${version_tag}/saltcorn.js` },
-          ];
-          if (state.getConfig("page_custom_css", ""))
-            headers.push({ style: state.getConfig("page_custom_css", "") });
-          if (state.getConfig("page_custom_html", ""))
-            headers.push({
-              headerTag: state.getConfig("page_custom_html", ""),
-            });
-          const role = (req.user || {}).role_id || 10;
-
-          const html = layout.wrap({
-            title: page.title,
-            brand: {},
-            menu: [],
-            currentUrl: "",
-            alerts: [],
-            body: contents,
-            headers,
-            role,
-            req,
-          });
-
-          console.log(html);
+          const html = await renderPage(contents, thePage, req);
+          let options = { format: "A4", path: "/tmp/page.pdf" };
+          return await generatePdf({ content: html }, options);
         }
       },
     },
   },
+};
+
+const renderPage = async (contents, page, req) => {
+  const state = getState();
+  const layout = state.getLayout(req.user);
+  const version_tag = db.connectObj.version_tag;
+
+  const headers = [
+    ...state.headers,
+    {
+      headerTag: `<script>var _sc_globalCsrf = "${req.csrfToken()}"; var _sc_version_tag = "${version_tag}";</script>`,
+    },
+    { css: `/static_assets/${version_tag}/saltcorn.css` },
+    { script: `/static_assets/${version_tag}/saltcorn.js` },
+  ];
+  if (state.getConfig("page_custom_css", ""))
+    headers.push({ style: state.getConfig("page_custom_css", "") });
+  if (state.getConfig("page_custom_html", ""))
+    headers.push({
+      headerTag: state.getConfig("page_custom_html", ""),
+    });
+  const role = (req.user || {}).role_id || 10;
+
+  return layout.wrap({
+    title: page.title,
+    brand: {},
+    menu: [],
+    currentUrl: "",
+    alerts: [],
+    body: contents,
+    headers,
+    role,
+    req,
+  });
 };
