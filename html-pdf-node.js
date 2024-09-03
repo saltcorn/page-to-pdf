d0c152b4fda2c895ce8500a77cf8a2965311b1c1
Tom Nielsen <tanielsen@gmail.com>
2024-09-03 18:00:46 +0100

    close browser in finally, no bluebird

--- html-pdf-node.js
+++ html-pdf-node.js
@@ -5,11 +5,10 @@ https://github.com/mrafiqk/html-pdf-node/
 */
 
 const puppeteer = require("@saltcorn/puppeteer-v17");
-var Promise = require("bluebird");
 const hb = require("handlebars");
 
 module.exports;
-async function generatePdf(file, options, callback) {
+async function generatePdf(file, options) {
   // we are using headless mode
   let args = ["--no-sandbox", "--disable-setuid-sandbox"];
   if (options.args) {
@@ -30,71 +29,16 @@ async function generatePdf(file, options, callback) {
     else await page.setCookie(options.cookie);
   }
 
-  if (file.content) {
-    console.log("Compiling the template with handlebars");
-    // we have compile our code with handlebars
-    const template = hb.compile(file.content, { strict: true });
-    const result = template(file.content);
-    const html = result;
-
-    // We set the page content as the generated html by handlebars
-    await page.setContent(html, {
-      waitUntil: "networkidle0", // wait for page to load completely
-    });
-  } else {
+  try {
     await page.goto(file.url, {
       waitUntil: "networkidle0", // wait for page to load completely
     });
+    const data = await page.pdf(options);
+
+    return Buffer.from(Object.values(data));
+  } finally {
+    await browser.close();
   }
-
-  return Promise.props(page.pdf(options))
-    .then(async function (data) {
-      await browser.close();
-
-      return Buffer.from(Object.values(data));
-    })
-    .asCallback(callback);
-}
-
-async function generatePdfs(files, options, callback) {
-  // we are using headless mode
-  let args = ["--no-sandbox", "--disable-setuid-sandbox"];
-  if (options.args) {
-    args = options.args;
-    delete options.args;
-  }
-  const browser = await puppeteer.launch({
-    args: args,
-  });
-  let pdfs = [];
-  const page = await browser.newPage();
-  for (let file of files) {
-    if (file.content) {
-      console.log("Compiling the template with handlebars");
-      // we have compile our code with handlebars
-      const template = hb.compile(file.content, { strict: true });
-      const result = template(file.content);
-      const html = result;
-      // We set the page content as the generated html by handlebars
-      await page.setContent(html);
-    } else {
-      await page.goto(file.url, {
-        waitUntil: "networkidle0", // wait for page to load completely
-      });
-    }
-    let pdfObj = JSON.parse(JSON.stringify(file));
-    delete pdfObj["content"];
-    pdfObj["buffer"] = Buffer.from(Object.values(await page.pdf(options)));
-    pdfs.push(pdfObj);
-  }
-
-  return Promise.resolve(pdfs)
-    .then(async function (data) {
-      await browser.close();
-      return data;
-    })
-    .asCallback(callback);
 }
 
 module.exports.generatePdf = generatePdf;
-module.exports.generatePdfs = generatePdfs;
